05e751f4929c4357a8aa846d2736910f1a89cc91
Sai kanala <sainath.vk.07@gmail.com>
2024-08-18 00:52:58 +0530

    Add PostgreSQL dependency and update Docker configuration

---
+++ app/app/tests.py
@@ -0,0 +1,15 @@
+
+
+from django.test import SimpleTestCase 
+
+from app import calc
+
+class ClacTests(SimpleTestCase):
+    def test_add_num(self):
+        res = calc.add(5,6)
+        
+        self.assertEqual(res,11)
+        
+    def test_subtract_num(self):
+        res = calc.subtract(10,15)
+        self.assertEqual(res,5)
